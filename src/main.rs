db742c2047d7594ac27938e5b14aeec3fca723fb
Jerome Cho <yourname@example.com>
2022-09-10 08:22:42 -0700

    Change project name

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+use multithreaded_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     // IP address of local computer (localhost) is 127.0.0.1
     // ::bind returns a new TcpListener instance
     // connecting to a port to listen to is called "binding to a port"
@@ -16,42 +21,26 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let status_line;
-    let contents; 
 
-    if request_line == "GET / HTTP/1.1" {
-        status_line = "HTTP/1.1 200 OK\r\n\r\n";
-        contents = fs::read_to_string("hello.html").unwrap();
-        // print_type_of(&response);
-        // * as_bytes - turns string data into bytes
-        // write_all seems to be like *send* in JavaScript
-    } else {
-        // some other request
-        status_line = "HTTP/1.1 404 NOTFOUND\r\n\r\n";
-        contents = fs::read_to_string("404.html").unwrap();
-        /*
-           let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
 
-        let response = format!(
-            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
-        );
-        // print_type_of(&response);
-        stream.write_all(response.as_bytes()).unwrap();
-        // * as_bytes - turns string data into bytes
-        // write_all seems to be like *send* in JavaScript
-          */
-    }
-    // TODO - refactor above to use tuple!
-
+    let contents = fs::read_to_string(&filename).unwrap();
     let length = contents.len();
     let response = format!(
         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
